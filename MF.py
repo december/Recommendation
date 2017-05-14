54dbaf5c4e6ec35b7d48a5274eb780f1355dc1ce
路云飞 <luyunfeicx@163.com>
2017-05-14 17:57:24 +0800

    fix bug

--- MF.py
+++ MF.py
@@ -48,7 +48,8 @@ n = len(answer)
 k = 50
 lbd = 0.01
 alpha = 0.0001
-J = 100
+lastJ = 0
+loss = 100
 X = train.todense()
 U = np.mat(np.random.normal(size=(10000, k)))
 V = np.mat(np.random.normal(size=(10000, k)))
@@ -57,25 +58,28 @@ cnt = 0
 iterlist = list()
 losslist = list()
 rmselist = list()
-while cnt < 2:
+while loss > 0.01:
 	delta = U * V.T - X
-	print delta[:3,:3]
+	#print delta[:3,:3]
 	D = np.multiply(A, delta)
-	print D[:3,:3]
+	#print D[:3,:3]
 	du = D * V + 2 * lbd * U
-	print du[:3,:3]
+	#print du[:3,:3]
 	dv = D.T * U + 2 * lbd * V
-	print dv[:3,:3]
+	#print dv[:3,:3]
 	U = U - alpha * du
-	print U[:3,:3]
+	#print U[:3,:3]
 	V = V - alpha * dv
-	print V[:3,:3]
+	#print V[:3,:3]
 	J = 0.5 * (np.linalg.norm(D) ** 2) + lbd * (np.linalg.norm(U) ** 2) + lbd * (np.linalg.norm(V) ** 2)
 	cnt += 1
 	iterlist.append(cnt)
 	print cnt
 	losslist.append(J)
 	print J
+	loss = abs(lastJ-J)
+	lastJ = J
+	print loss
 	Y = U * V.T
 	rmse = 0
 	for i in range(n):
